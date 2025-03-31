df7b324036a8ead78b804a696fe86cab251cc66b
guilherme-alc <guilhermealc01@gmail.com>
2025-03-31 19:19:43 -0300

    feat: adiciona entidades de serialização e desserialização

---
+++ Models/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿namespace GeradorPeriodosAquisitivos.Models
+{
+    public class Funcionario
+    {
+        public int CodEmpresa { get; set; }
+        public string CpfFuncionario { get; set; }
+        public DateTime DataAdmissao { get; set; }
+    }
+}
---
+++ Models/PeriodoAquisitivo.cs
@@ -0,0 +1,10 @@
+﻿namespace GeradorPeriodosAquisitivos.Models
+{
+    public class PeriodoAquisitivo
+    {
+        public int CodEmpresa { get; set; }
+        public string CpfFuncionario { get; set; }
+        public DateTime DataInicioPeriodo { get; set; }
+        public DateTime DataFimPeriodo { get; set; }
+    }
+}
